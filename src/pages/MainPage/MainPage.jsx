a4fa0ffa0a6f0d90850e74ff8922e223dd3bc476
Alexgbp <bboy91@bk.ru>
2024-03-18 20:04:26 +0300

    start

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,8 +2,16 @@ import styles from "./MainPage.module.css";
 
 export const MainPage = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>MainPage</h1>
-    </div>
+    <main className={styles.main}>
+      <section className={styles.top}>
+        <div className={styles.top__first}>
+          <h3 className={styles.top__sub_title}>Онлайн-тренировки для занятий дома</h3>
+          <h1 className={styles.top__title}>Начните заниматься спортом и улучшите качество жизни</h1>
+        </div>
+        <div className={styles.top__second}>
+            <img className={styles.top__svg} src="images/mainPagePictures/change_your_body_logo.svg" alt="change your body" />
+        </div>
+      </section>
+    </main>
   );
 };
