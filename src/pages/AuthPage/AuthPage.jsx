66682f237392da619cb949a646f0ede82e1748fa
Christinnenko <Christinnenko@yandex.ru>
2024-03-18 22:06:48 +0300

    feat: add auth&login page layout

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,9 +1,70 @@
 import styles from "./AuthPage.module.css";
 
 export const AuthPage = () => {
+  const isLoginMode = false;
+  const isLoading = false;
+
   return (
-    <div>
-      <h1 className={styles.title}>Pages was not found :c</h1>
+    <div className={styles.pageContainer}>
+      <div className={styles.modalForm}>
+        <div className={styles.modalLogo}>
+          <img src="../images/header_logo_black.png" alt="logo" />
+        </div>
+
+        {isLoginMode ? (
+          <>
+            <div className={styles.modalInputs}>
+              <input
+                className={styles.modalInput}
+                type="text"
+                name="login"
+                placeholder="Логин"
+              />
+              <input
+                className={styles.modalInput}
+                type="password"
+                name="password"
+                placeholder="Пароль"
+              />
+            </div>
+            <div className={styles.buttons}>
+              <button className={styles.primaryButton} disabled={isLoading}>
+                {isLoading ? "Загрузка..." : "Войти"}
+              </button>
+
+              <button className={styles.secondaryButton}>
+                Зарегистрироваться
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.modalInputs}>
+              <input
+                className={styles.modalInput}
+                type="text"
+                name="login"
+                placeholder="Логин"
+              />
+              <input
+                className={styles.modalInput}
+                type="password"
+                name="password"
+                placeholder="Пароль"
+              />
+              <input
+                className={styles.modalInput}
+                type="password"
+                name="repeat-password"
+                placeholder="Повторите пароль"
+              />
+            </div>
+            <button className={styles.primaryButton} disabled={isLoading}>
+              {isLoading ? "Регистрация..." : "Зарегистрироваться"}
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
