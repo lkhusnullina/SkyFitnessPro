754128bea7cd0c37a4725e519c35449e13454f20
Vera869 <verea8910@gmail.com>
2024-03-19 00:02:50 +0700

    +

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,13 @@ function Header(){
     return (
         <div>
             <div className={styles.header_block}>
-                <img src="images/logo.png" />
-                <button className={styles.header_btn}>Войти</button>
+                <img className={styles.header_logo} src="images/logo.png" />
+                <div className={styles.account_block}>
+                  <img className={styles.account_avatar} />
+                  <p className={styles.account_nickname}>Сергей</p>
+                </div>
+                
+                {/* <button className={styles.header_btn}>Войти</button> */}
             </div>
         </div>
     )
