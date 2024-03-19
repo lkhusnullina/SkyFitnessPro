a86dc85c434e1a0d379ac6547812a08c57c5da1f
Alexgbp <bboy91@bk.ru>
2024-03-19 22:10:18 +0300

    initial

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,7 +3,7 @@ import styles from "./MainPage.module.css";
 export const MainPage = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>MainPage</h1>
+
     </div>
   );
 };
