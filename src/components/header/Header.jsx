b3cbd5e4a41eafcffb389c6a7264b932ef7d1cf3
Любовь Хуснуллина <lyuba140296@gmail.com>
2024-03-21 20:59:56 +0500

    смена элементов хедера согласно заданному маршруту

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,37 @@
 import { useLocation } from "react-router-dom";
 import styles from "./Header.module.css";
 
-function Header(){
-    const location = useLocation();
-    const home = location.pathname === "/";
-    console.log(home);
-    const logoUrl = home ? "images/header_logo.png" : "images/header_logo_black.png";
-    return (
-        <div>
-            <div className={styles.header_block}>
-                <img src={logoUrl} />
-                <button className={styles.header_btn}>Войти</button>
-            </div>
-        </div>
-    )
-  };
+function Header() {
+  const location = useLocation();
+  const home = location.pathname === "/";
+  const logoUrl = home
+    ? "images/header_logo.png"
+    : "images/header_logo_black.png";
 
-  export default Header 
+  const noUser = location.pathname === "/yoga";
+  const userProfile =
+    location.pathname === "/profile" || location.pathname === "/workout";
+
+  const userUrl = noUser ? (
+    <div></div>
+  ) : userProfile ? (
+    <div className={styles.header_user}>
+      <img src="images/avatar.svg" alt="avatar" />
+      <div className={styles.header_userName}>Сергей</div>
+      <img src="images/arrow.svg" alt="avatar" />
+    </div>
+  ) : (
+    <button className={styles.header_btn}>Войти</button>
+  );
+
+  return (
+    <div>
+      <div className={styles.header_block}>
+        <img src={logoUrl} />
+        <div>{userUrl}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
