ef40a13f3884035c892f143a2333c5183e6829f9
Любовь Хуснуллина <lyuba140296@gmail.com>
2024-03-22 22:31:59 +0500

    fix

--- src/pages/WorkoutVideoPage/WorkoutVideoPage.jsx
+++ src/pages/WorkoutVideoPage/WorkoutVideoPage.jsx
@@ -13,33 +13,33 @@ export const WorkoutVideoPage = () => {
   return (
     <div>
       <h2 className={styles.title}></h2>
-      <div className={styles.description}>   /     / 2 </div>
+      <div className={styles.description}>Красота и здоровье / Йога на каждый день / 2 день</div>
       <Video/>
       <div className={styles.workout_block}>
         <div className={styles.workout_description}>
-            <h2 className={styles.workout_header}></h2>
+          <h2 className={styles.workout_header}>Упражнения</h2>
             <ul className={styles.workout_element}>
-                <li>  (10 )</li>
-                <li>  (10 )</li>
-                <li> ,    <br/>(5 )</li>
+                <li>Наклон вперед (10 повторений)</li>
+                <li>Наклон назад (10 повторений)</li>
+                <li>Поднятие ног, согнутых в коленях <br/>(5 повторений)</li>
             </ul>
             <button className={styles.button_progress} onClick={() => setIsOpen(true)}>  </button>
         </div>
         <div className={styles.progress_block}>
-            <div className={styles.progress_header}>    2:</div>
+            <div className={styles.progress_header}>Мой прогресс по тренировке 2:</div>
             <div className={styles.progress_percentages}>
                 <div className={styles.progress_percent}>
-                    <div className={styles.progress_percent__name}> </div>
+                    <div className={styles.progress_percent__name}>Наклоны вперед</div>
                     <progress className={styles.progress_percent__value1} value="45" max="100"></progress>
                     <span className={styles.progress_percent__count}>45%</span>
                 </div>
                 <div className={styles.progress_percent}>
-                    <div className={styles.progress_percent__name}> </div>
+                    <div className={styles.progress_percent__name}>Наклоны назад</div>
                     <progress  className={styles.progress_percent__value2} value="45" max="100"></progress>
                     <span className={styles.progress_percent__count}>45%</span>
                 </div>
                 <div className={styles.progress_percent}>
-                    <div className={styles.progress_percent__name}> , <br />   </div>
+                    <div className={styles.progress_percent__name}>Поднятие ног, <br /> согнутых в коленях</div>
                     <progress className={styles.progress_percent__value3} value="45" max="100"></progress>
                     <span className={styles.progress_percent__count}>45%</span>
                 </div>
