260f64ba67100c9a405859e31fbb7cb7860d9006
Любовь Хуснуллина <lyuba140296@gmail.com>
2024-03-23 08:34:20 +0500

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,18 +4,34 @@ import styles from "./Header.module.css";
 function Header(){
     const location = useLocation();
     const home = location.pathname === "/";
-    console.log(home);
-    const logoUrl = home ? "images/header_logo.png" : "images/header_logo_black.png";
+    const logoUrl = home
+      ? "images/header_logo.png"
+      : "images/header_logo_black.png";
+  
+    const noUser = location.pathname === "/yoga";
+    const userProfile =
+      location.pathname === "/profile" || location.pathname === "/workout";
+  
+    const userUrl = noUser ? (
+      <div></div>
+    ) : userProfile ? (
+      <div className={styles.header_user}>
+        <img src="images/avatar.svg" alt="avatar" />
+        <div className={styles.header_userName}>Сергей</div>
+        <img src="images/arrow.svg" alt="arrow" />
+      </div>
+    ) : (
+        <Link to="/login"><button className={styles.header_btn}>Войти</button></Link>
+    );
+  
     return (
-        <div id="top">
-            <div className={styles.header_block}>
-                <img src={logoUrl} />
-                <Link to="/login">
-                <button className={styles.header_btn}>Войти</button>
-                </Link>
-            </div>
+      <div id="top">
+        <div className={styles.header_block}>
+            <Link to="/"><img src={logoUrl} /></Link>
+            <div>{userUrl}</div>
         </div>
-    )
+      </div>
+    );
   };
 
   export default Header 
