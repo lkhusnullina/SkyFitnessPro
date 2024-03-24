51e2ee25f47e81192054f84dfc5b5a1fc3b02e7e
Alexgbp <bboy91@bk.ru>
2024-03-24 14:58:44 +0300

    chooseLesson

--- src/components/modals/ModalChooseLesson.jsx
+++ src/components/modals/ModalChooseLesson.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import styles from "./ModalChooseLesson.module.css";
+import { useNavigate } from "react-router-dom";
 
 function ModalChooseLesson({closeModal}) {
+  const navigate = useNavigate()
+
+  const clickChooseLesson = () => {
+    navigate("/workout")
+  }
 
   const handleClickOutside = (event) => {
     if (event.target.classList.contains(styles.pageContainer)) {
@@ -16,27 +22,27 @@ function ModalChooseLesson({closeModal}) {
         <h1 className={styles.title}>Выберите тренировку</h1>
         <div className={styles.lessonsContainerScroll}>
           <div className={styles.lessonsContainer}>
-            <div className={styles.lessonContainer}>
+            <div onClick={clickChooseLesson} className={styles.lessonContainer}>
               <h2 className={styles.lessonTitle}>Утренняя практика</h2>
               <p className={styles.lesson}>Йога на каждый день / 1 день</p>
             </div>
-            <div className={styles.lessonContainer}>
+            <div onClick={clickChooseLesson} className={styles.lessonContainer}>
               <h2 className={styles.lessonTitle}>Красота и здоровье</h2>
               <p className={styles.lesson}>Йога на каждый день / 2 день</p>
             </div>
-            <div className={styles.lessonContainer}>
+            <div onClick={clickChooseLesson} className={styles.lessonContainer}>
               <h2 className={styles.lessonTitle}>Асаны стоя</h2>
               <p className={styles.lesson}>Йога на каждый день / 3 день</p>
             </div>
-            <div className={styles.lessonContainer}>
+            <div onClick={clickChooseLesson} className={styles.lessonContainer}>
               <h2 className={styles.lessonTitle}>Растягиваем мышцы бедра</h2>
               <p className={styles.lesson}>Йога на каждый день / 4 день</p>
             </div>
-            <div className={styles.lessonContainer}>
+            <div onClick={clickChooseLesson} className={styles.lessonContainer}>
               <h2 className={styles.lessonTitle}>Гибкость спины</h2>
               <p className={styles.lesson}>Йога на каждый день / 5 день</p>
             </div>
-            <div className={styles.lessonContainer}>
+            <div onClick={clickChooseLesson} className={styles.lessonContainer}>
               <h2 className={styles.lessonTitle}>Утренняя практика</h2>
               <p className={styles.lesson}>Йога на каждый день / 6 день</p>
             </div>
