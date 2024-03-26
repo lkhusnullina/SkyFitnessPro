ae2104334e4a6c3644b926a2db0ec98c66719fd2
Любовь Хуснуллина <lyuba140296@gmail.com>
2024-03-26 21:04:20 +0500

    Подключила прогрессБар

--- src/pages/WorkoutVideoPage/WorkoutVideoPage.jsx
+++ src/pages/WorkoutVideoPage/WorkoutVideoPage.jsx
@@ -3,6 +3,7 @@ import ModalMyProgress from '../../components/modals/ModalMyProgress/ModalMyProg
 import Video from '../../components/workoutVideo/WorkoutVideo'
 import styles from './WorkoutVideoPage.module.css'
 import Lessons from '../../components/lessons/Lessons.jsx'
+import { Progress } from '../../components/progress/Progress.jsx'
 
 export const WorkoutVideoPage = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -20,44 +21,7 @@ export const WorkoutVideoPage = () => {
       <Video />
       <div className={styles.workout_block}>
         <Lessons setIsOpen={setIsOpen}/>
-        <div className={styles.progress_block}>
-          <div className={styles.progress_header}>
-            Мой прогресс по тренировке 2:
-          </div>
-          <div className={styles.progress_percentages}>
-            <div className={styles.progress_percent}>
-              <div className={styles.progress_percent__name}>
-                Наклоны вперед
-              </div>
-              <progress
-                className={styles.progress_percent__value1}
-                value="45"
-                max="100"
-              ></progress>
-              <span className={styles.progress_percent__count}>45%</span>
-            </div>
-            <div className={styles.progress_percent}>
-              <div className={styles.progress_percent__name}>Наклоны назад</div>
-              <progress
-                className={styles.progress_percent__value2}
-                value="45"
-                max="100"
-              ></progress>
-              <span className={styles.progress_percent__count}>45%</span>
-            </div>
-            <div className={styles.progress_percent}>
-              <div className={styles.progress_percent__name}>
-                Поднятие ног, <br /> согнутых в коленях
-              </div>
-              <progress
-                className={styles.progress_percent__value3}
-                value="45"
-                max="100"
-              ></progress>
-              <span className={styles.progress_percent__count}>45%</span>
-            </div>
-          </div>
-        </div>
+        <Progress/>
       </div>
       {isOpen && <ModalMyProgress isOpen={isOpen} closeModal={closeModal} />}
     </div>
