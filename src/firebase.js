b54c137d6bd94cf7dd4568ee11e96aaacf64d632
Vera869 <verea8910@gmail.com>
2024-03-27 19:58:07 +0700

    +firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app'
+// // import { getAuth, onAuthStateChanged } from 'firebase/auth'
+// // import { collection, getDocs, getFirestore } from 'firebase/firestore'
+// // import { } from 'firebase/<service>';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyBAa9OxprPu2EuJEl5vvoRqIzGJ9-FSK48",
+  authDomain: "fitness-project-bc4c2.firebaseapp.com",
+  databaseURL: "https://fitness-project-bc4c2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fitness-project-bc4c2",
+  storageBucket: "fitness-project-bc4c2.appspot.com",
+  messagingSenderId: "155881875437",
+  appId: "1:155881875437:web:5c0638eea857109e04a9e4"
+})
+const db = getFirestore(firebaseApp);
+const querySnapshot = await getDocs(collection(db, "courses"));
+console.log(querySnapshot)
+
+
+export default db 
--- src/pages/CoursePage/CoursePage.jsx
+++ src/pages/CoursePage/CoursePage.jsx
@@ -4,8 +4,21 @@ import Footer from '../../components/CourseDescription/CourseFooter/CourseFooter
 import Points from '../../components/CourseDescription/CoursePoints/CoursePoints'
 import Types from '../../components/CourseDescription/CourseTypes/CourseTypes'
 import styles from './CoursePage.module.css'
+import {db} from '../../firebase'
+import { collection, getDocs } from 'firebase/firestore'
 
 export const CoursePage = () => {
+
+// const querySnapshot = await getDocs(collection(db, "courses"));
+// querySnapshot.forEach((doc) => {
+//   console.log(doc.data());
+// });
+// const cursesCol = collection(db, 'courses')
+// const snapshot = getDocs(cursesCol)
+// const yoga = snapshot.ab1c3f
+// console.log(db)
+// console.log(snapshot)
+// console.log(yoga)
   return (
     <div className={styles.container}>
       <Banner />
