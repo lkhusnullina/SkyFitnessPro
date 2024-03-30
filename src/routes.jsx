001e5811cdcfab7f81bb9e76467175034c82b149
Vera869 <verea8910@gmail.com>
2024-03-30 22:05:18 +0700

    + isAllowed

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,14 +7,36 @@ import { ProfilePage } from './pages/ProfilePage/ProfilePage.jsx'
 import { CoursePage } from './pages/CoursePage/CoursePage.jsx'
 import { WorkoutVideoPage } from './pages/WorkoutVideoPage/WorkoutVideoPage.jsx'
 import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage.jsx'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase.js'
+import { useDispatch, useSelector } from 'react-redux'
+import { setIsAllowed } from './store/slice.jsx'
+import { useEffect } from 'react'
 
 export const AppRoutes = () => {
+
+  const dispatch = useDispatch()
+  //Отслеживаем состояние авторизации
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user !== null) {
+        dispatch(setIsAllowed(true))
+        console.log("logged in!")
+      } else {
+        dispatch(setIsAllowed(false))
+        console.log("no user");
+      }
+    })
+  }, [dispatch])
+
+  const isAllowed = useSelector(store => store.courses.isAllowed);
+  console.log(isAllowed);
   return (
     <Routes>
       <Route path="/" element={<LayoutPage />}>
         <Route index element={<MainPage />} />
         <Route path="/course" element={<CoursePage />} />
-        <Route element={<ProtectedRoute isAllowed={Boolean(true)} />}>
+        <Route element={<ProtectedRoute isAllowed={isAllowed} />}>
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/workout" element={<WorkoutVideoPage />} />
         </Route>
--- src/store/slice.jsx
+++ src/store/slice.jsx
@@ -5,8 +5,13 @@ const coursesSlice = createSlice({
   name: 'courses',
   initialState: {
     cards: Cards,
+    isAllowed: false,
   },
-  reducers: {},
+  reducers: {
+    setIsAllowed ( state, action ) {
+      state.isAllowed = action.payload
+    }
+  },
 })
-
+export const {setIsAllowed} = coursesSlice.actions
 export const coursesReducer = coursesSlice.reducer
