fb21fd62923a4cdc900e798ac56581a3e173a941
Christinnenko <Christinnenko@yandex.ru>
2024-04-05 22:12:57 +0300

    feat: add disable for button, error massage, close list

--- src/components/buttons/bigButton.jsx
+++ src/components/buttons/bigButton.jsx
@@ -1,14 +1,11 @@
-import styles from './bigButton.module.css';
+import styles from './bigButton.module.css'
 
-export const BigButton = ({value, onClick}) => {
-   return (
-      <button
-      className={styles.button}
-      onClick={onClick}
-    >
+export const BigButton = ({ value, onClick, isLoading }) => {
+  return (
+    <button className={styles.button} onClick={onClick} disabled={isLoading}>
       {value}
     </button>
-   )
+  )
 }
 
 //<BigButton value='test' onClick={onClick} />
